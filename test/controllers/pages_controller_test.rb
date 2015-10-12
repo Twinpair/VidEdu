02b11069f01b0dafcc113cdbda4a32762254684c
Jonathan Raxa <jonathanraxa@gmail.com>
2015-10-12 15:44:11 -0700

    trival

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get splash" do
+    get :splash
+    assert_response :success
+  end
+
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get profile" do
+    get :profile
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
+
+  test "should get features" do
+    get :features
+    assert_response :success
+  end
+
+  test "should get subjects" do
+    get :subjects
+    assert_response :success
+  end
+
+end
