c1f9e1813c528b6516379a7d201e82064ab6160a
Jonathan Raxa <jonathanraxa@gmail.com>
2015-10-13 09:55:38 -0700

    created pages and controllers. Navigation to different pages created

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,28 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def home
+  end
+
+  def about
+  end
+
+  def subjects
+  end
+
+  def profile
+  end
+
+  def contact
+  end
+
+  def featured
+  end
+
+  def faq
+  end
+
+  def splash
+  end
+end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get subjects" do
+    get :subjects
+    assert_response :success
+  end
+
+  test "should get profile" do
+    get :profile
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
+
+  test "should get featured" do
+    get :featured
+    assert_response :success
+  end
+
+  test "should get faq" do
+    get :faq
+    assert_response :success
+  end
+
+  test "should get splash" do
+    get :splash
+    assert_response :success
+  end
+
+end
