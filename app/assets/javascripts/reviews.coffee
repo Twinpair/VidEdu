1b6aaca31a2692c1ca2cfb7140c1784a34d2a4b8
Eric <twinpaireric@gmail.com>
2017-09-11 17:16:49 -0700

    Remove unnecessary javascript files

--- app/assets/javascripts/reviews.coffee
+++
@@ -1,5 +0,0 @@
-$(document).on "page: change", ->
-  $('#reviews-link').click (event) ->
-    event.preventDefault()
-    $('#reviews-section').fadeToggle()
-    $('#review_body').focus()
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,18 +10,7 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-#Precompiled applications CSS and JS
-Rails.application.config.assets.precompile += %w( bootstrap.js )
-Rails.application.config.assets.precompile += %w( jquery-1.11.0.js)
-Rails.application.config.assets.precompile += %w( bootstrap.min.js )
-Rails.application.config.assets.precompile += %w( jquery.easing.min.js )
-
-
 #Precompiled CSS and JS for pages/home
-Rails.application.config.assets.precompile += %w( modernizr.custom.js )
-Rails.application.config.assets.precompile += %w( jquery.1.11.1.js )
-Rails.application.config.assets.precompile += %w( bootstrap.js )
-Rails.application.config.assets.precompile += %w( SmoothScroll.js )
 Rails.application.config.assets.precompile += %w( jquery.isotope.js )   
 Rails.application.config.assets.precompile += %w( owl.carousel.js ) 
 Rails.application.config.assets.precompile += %w( main.js )
